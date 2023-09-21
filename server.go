00ffcf05bd357e695bba64c0df0276040809ebaa
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-09-21 12:57:17 +0200

    First commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New(
+		fiber.Config{
+			AppName:       "v-lock API Server (Golang)",
+			CaseSensitive: true,
+			ServerHeader:  "v-lock API Server (Golang)",
+		})
+
+	app.Static("/", "./public")
+
+	app.Listen(":3000")
+}
