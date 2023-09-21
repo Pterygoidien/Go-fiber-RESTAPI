dc071121a7397ad04f049beefcb51fc124850bac
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-09-21 13:01:41 +0200

    GLOBAL| Add router.go, main router (dispatcher)

--- router.go
+++ router.go
@@ -1,1 +1,9 @@
-controller := &controllers.UserController{}
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func mainRouter(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World from router.go!")
+	})
+}
