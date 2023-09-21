e331efbfd1fe7ce56050a21483fcdc09a4df044e
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-09-21 13:31:15 +0200

    ROUTER| Link authRouter to main router

--- router.go
+++ router.go
@@ -2,8 +2,11 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
-func mainRouter(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
+func mainRouter(api fiber.Router) {
+	api.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World from router.go!")
 	})
+
+	api.Group("/auth", authRouter)
+
 }
